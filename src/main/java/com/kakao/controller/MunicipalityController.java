f1dfdc69f4365b9ebcf098dd4f1950c2b3eaca60
daehyun <dleo9307@naver.com>
2020-03-31 03:40:07 +0900

    findByRegion 기능 추가

--- src/main/java/com/kakao/controller/MunicipalityController.java
+++ src/main/java/com/kakao/controller/MunicipalityController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
 
@@ -29,7 +30,7 @@ public class MunicipalityController {
 
 
     //CSV 데이터셋을 데이터베이스에 저장하는 API
-    @GetMapping(path="/saveDataSet")
+    @GetMapping("/saveDataSet")
     public ResponseEntity<String> saveDataSet(){
         if(municipalityService.findAll().size() > 0){
             return new ResponseEntity("Already inserted", HttpStatus.BAD_REQUEST);
@@ -39,16 +40,27 @@ public class MunicipalityController {
         }
     }
 
-    //모든 지자체 목록을 조회하는 APi
-    @GetMapping(path = "/findAll")
+    //모든 지자체 목록을 조회하는 API
+    @GetMapping("/findAll")
     public ResponseEntity<List> findAll(){
         return new ResponseEntity(municipalityService.findAll(), HttpStatus.OK);
     }
 
 
     //지자체 이름으로 목록을 조회하는 API
-    @GetMapping(path = "/findByRegion")
+    @GetMapping("/findByRegion")
     public ResponseEntity<SupportInform> findByRegion(@RequestParam HashMap<String,Object> req){
         return new ResponseEntity(municipalityService.findByRegion(req.get("region").toString()), HttpStatus.OK);
     }
+
+    //지자체 아이디로 지자체를 업데이트 하는 API
+    @PutMapping("/update/{region}")
+    public ResponseEntity<SupportInform> updateByRegion(@PathVariable(value = "region") String region, @RequestBody SupportInform reqObj){
+       final SupportInform result = municipalityService.updateByRegion(region, reqObj);
+        if (result == null) return new ResponseEntity("NOT Find Region", HttpStatus.NOT_FOUND);
+        else{
+            return new ResponseEntity(result, HttpStatus.OK);
+        }
+    }
+
 }
--- src/main/java/com/kakao/service/MunicipalityService.java
+++ src/main/java/com/kakao/service/MunicipalityService.java
@@ -23,4 +23,20 @@ public class MunicipalityService {
         return supportInformRepository.findBySupportAgencyRegion(region);
     }
 
+    public SupportInform updateByRegion(final String region, final SupportInform reqObj){
+        SupportInform supportInform = findByRegion(region);
+
+        supportInform.setTargetValue(reqObj.getTargetValue());
+        supportInform.setSupportAgency(reqObj.getSupportAgency());
+        supportInform.setMgmt(reqObj.getMgmt());
+        supportInform.setInstitute(reqObj.getInstitute());
+        supportInform.setRate(reqObj.getRate());
+        supportInform.setLimitValue(reqObj.getLimitValue());
+        supportInform.setUsage(reqObj.getUsage());
+        supportInform.setReception(reqObj.getReception());
+        supportInform.setSupportAgency(reqObj.getSupportAgency());
+
+        return supportInformRepository.save(supportInform);
+    }
+
 }
